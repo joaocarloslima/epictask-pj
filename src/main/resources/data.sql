380152133cc0c41efea4067a453245675a266cc3
Joao <joaocarloslima@me.com>
2023-10-02 11:59:57 -0300

    projeto inicial com thymeleaf

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO task (title, description, score) VALUES ('Prototipo', 'Criar as telas no figma', 50);
+INSERT INTO task (title, description, score) VALUES ('BD', 'Modelar o banco de dados', 20);
+INSERT INTO task (title, description, score) VALUES ('Teste', 'Criar testes automatizados', 20);
